2940e449da50f8b9c25e1bb14e9d03e83e21ec40
jdltechworks <jo@bigzeta.com>
2017-11-14 17:59:04 +0800

    Susy starts

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,23 +3,27 @@ import React, { Component } from 'react'
 class NavBar extends Component {
     render() {
         return(
-            <div className="navbar-main">
-                <div className="container grid-xl">
-                    <header className="navbar">
-                      <section className="navbar-section">
-                        <a href="#" className="navbar-brand">
-                            <img src="https://dummyimage.com/170x50/000/fff" />
-                        </a>
-                      </section>
-                      <section className="navbar-section">
-                          <div className="input-group input-inline">
-                            <input className="form-input" type="text" placeholder="search" />
-                            <button className="btn btn-primary btn-action input-group-btn"><i className="icon icon-search"></i></button>
-                          </div>
-                      </section>
-                    </header>
+            <header className="header">
+                <div className="navbar">
+                  <section className="navbar-brand">
+                    <a href="#" className="navbar-brand-link">
+                        <img src="https://dummyimage.com/170x50/000/fff" />
+                    </a>
+                  </section>
+                  <section className="navbar-section">
+                      <div className="input-group input-inline">
+                        <input className="form-input" type="text" placeholder="search" />
+                        <button className="btn btn-primary btn-action input-group-btn"><i className="icon icon-search"></i></button>
+                      </div>
+                  </section>
+                  <section className="navbar-section-last">
+                      <div className="input-group input-inline">
+                        <input className="form-input" type="text" placeholder="search" />
+                        <button className="btn btn-primary btn-action input-group-btn"><i className="icon icon-search"></i></button>
+                      </div>
+                  </section>
                 </div>
-            </div>
+            </header>
         )
     }
 }
